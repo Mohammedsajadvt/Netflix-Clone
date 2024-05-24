1f6533146c89bc5725c57d0de082cf4b374c1168
Mohammed Sajad vt <mohammedsajadvt@gmail.com>
2024-05-24 14:39:52 +0530

    "Home Page 1st Part"

--- lib/core/constant_values.dart
+++ lib/core/constant_values.dart
@@ -1,8 +1,14 @@
 import 'package:flutter_svg/flutter_svg.dart';
+
 // width
 const width40 = 40.0;
+const width20 = 20.0;
 // height
 const height50 = 50.0;
 const height20 = 20.0;
 // svg
-final addIcon =  SvgPicture.asset('images/add-icon.svg',);
+final addIcon = SvgPicture.asset(
+  'images/add-icon.svg',
+);
+// font
+const size20 = 20.0;
--- lib/presentation/pages/home/screen_home.dart
+++ lib/presentation/pages/home/screen_home.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/core/constant_values.dart';
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      backgroundColor: Colors.transparent,
-      leading: Image.asset('images/n.png'),
-      actions: [
-      Expanded(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Scaffold(
+      body: Stack(
         children: [
-         Text('Tv Shows'),
-         Text('Movies'),
-         Text('My List')
-          ],
-        ),
-      )
-      ],
-    ),);
+          SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height/2,
+              child: Image.asset('images/del.png',
+                fit: BoxFit.cover,
+              )),
+          SizedBox(
+            height: MediaQuery.of(context).size.height/6,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset('images/n.png'),
+                const SizedBox(width: width20,),
+                const Text('Tv Shows',style: TextStyle(fontSize: size20),),
+                 const SizedBox(width: width20,),
+                const Text('Movies',style: TextStyle(fontSize: size20),),
+                 const SizedBox(width: width20,),
+                const Text('My List',style: TextStyle(fontSize: size20),),
+                SizedBox(width: MediaQuery.of(context).size.width/10,)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
